945a76fe8789b8fbfd051e13a85fb2eb52907bf6
Preprocessing - I <karenly1961@gmail.com>
2019-01-24 16:53:39 -0500

    Lambda Classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,39 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrib) {
+        this.name = attrib.name;
+        this.age = attrib.age;
+        this.location = attrib.location;
+        this.gender = attrib.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttrib) {
+    super(instructorAttrib);
+    this.specialty =   instructorAttrib.specialty;
+    this.favLanguage = instructorAttrib.favLanguage;
+    this.catchPhrase = instructorAttrib.catchPhrase;
+    }
+}
+
+class Student extends Person {
+constructor(studentAttrib) {
+    super(studentAttrib);
+    this.previousBackground = studentAttrib.previousBackground;
+    this.className = studentAttrib.className;
+    this.favSubjects = studentAttrib.favSubjects;
+}
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttrib);{
+    super(pmAttrib);
+    this.gradClassName = pmAttrib.gradClassName;
+    this.favInstructor = pmAttrib.favInstructor;
+
+    };
+}
