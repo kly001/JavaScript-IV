fbd4a6efced74d1c8b80d26444378b00027af44c
Preprocessing - I <karenly1961@gmail.com>
2019-01-24 17:23:23 -0500

    Setting Up Instructors, Students and Project Managers

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,14 +26,49 @@ constructor(studentAttrib) {
     this.previousBackground = studentAttrib.previousBackground;
     this.className = studentAttrib.className;
     this.favSubjects = studentAttrib.favSubjects;
-}
+    }
 }
 
 class ProjectManager extends Instructor {
-    constructor(pmAttrib);{
+    constructor(pmAttrib) {
     super(pmAttrib);
     this.gradClassName = pmAttrib.gradClassName;
     this.favInstructor = pmAttrib.favInstructor;
 
-    };
+    }
 }
+
+const joan = new Instructor({
+    specialty:  "Ruby",
+    favLanguage: "JavaScript",
+    catchPhrase:"Ae you sure that's what you want to do?",
+
+})
+
+const edgar = new Instructor({
+    specialty: "Scratch",
+    favLanguage: "CSS",
+    catchPhrase: "Try it yourself",
+})
+
+const susan = new  Student({
+        previousBackground: "personal assistant",
+        className: "WEB21",
+        favoriteSubjects: ["Ruby", "Perl","Html"]
+})
+
+const john = new  Student({
+    previousBackground: "policeman",
+    className: "CS107",
+    favoriteSubjects: ["JavaScript", "CSS","Redux"]
+})
+
+const pearl = new ProjectManager ({
+    gradClass: "CS107",
+    favInstructor: "Edgar",
+})
+
+const mark = new ProjectManager ({
+    gradClass: "WEB21",
+    favInstructor: "Joan",
+})
